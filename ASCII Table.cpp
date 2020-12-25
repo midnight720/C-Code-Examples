b60b2a08c596531b8faf8343395ad75336d1cccc
Sanjan Geet Singh <sanjangeet720@gmail.com>
2020-12-25 13:27:33 +0530

    Added 'ASCII Table.cpp'

---
+++ ASCII Table.cpp
@@ -0,0 +1,13 @@
+/**
+ * Program to print ASCII Table
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i=0; i<256; i++) {
+        cout << i << " : " << char(i) << endl;
+    }
+    return 0;
+}

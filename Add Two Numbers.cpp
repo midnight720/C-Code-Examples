c3228049db89d12af89de296bfe6e2fa873ed5b9
Sanjan Geet Singh <sanjangeet720@gmail.com>
2020-12-25 12:37:59 +0530

    Added 'Add Two Numbers.cpp'

---
+++ Add Two Numbers.cpp
@@ -0,0 +1,18 @@
+/**
+ * Inputs Two Numbers and Print their Sum.
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    float n1, n2, sum;
+
+    cout << "Enter Two Numbers: ";
+    cin >> n1 >> n2;
+
+    sum = n1 + n2;
+    cout << "Sum of These Two Numbers is " << sum << endl;
+
+    return 0;
+}

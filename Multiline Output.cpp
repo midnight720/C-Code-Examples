0a4917992ce9fcad30682127b004157c6fc458e3
Sanjan Geet Singh <sanjangeet720@gmail.com>
2020-12-25 12:55:44 +0530

    Added 'Multiline Output.cpp'

---
+++ Multiline Output.cpp
@@ -0,0 +1,13 @@
+/**
+ * Program to print multiple lines using single cout.
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Maths = 90\n"
+         << "Physics = 77\n"
+         << "Chemistry = 69";
+    return 0;
+}
